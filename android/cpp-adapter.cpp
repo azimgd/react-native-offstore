743bcef00a26863f4da17d7da4bc1061d21e4f4a
azimgd <me@azimgd.com>
2023-01-22 02:47:30 +0500

    chore: fix Android build by adding temporary directory path, integrate folly on android via cmake

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -5,7 +5,8 @@
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_offstore_OffstoreModule_nativeSetup(JNIEnv *env, jclass type, long jsiPtr) {
+Java_com_offstore_OffstoreModule_nativeSetup(JNIEnv *env, jclass type, long jsiPtr, facebook::jni::alias_ref<facebook::react::CallInvokerHolder::javaobject> jsCallInvokerHolder, facebook::jni::alias_ref<facebook::jni::JString> temporaryDirectory) {
     auto runtime = reinterpret_cast<facebook::jsi::Runtime*>(jsiPtr);
-    offstore::setup(*runtime);
+    auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
+    offstore::setup(*runtime, temporaryDirectory->toStdString());
 }
--- android/src/main/java/com/offstore/OffstoreModule.java
+++ android/src/main/java/com/offstore/OffstoreModule.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.turbomodule.core.CallInvokerHolderImpl;
 
 @ReactModule(name = OffstoreModule.NAME)
 public class OffstoreModule extends NativeOffstoreSpec {
@@ -23,11 +24,15 @@ public class OffstoreModule extends NativeOffstoreSpec {
     System.loadLibrary("offstore");
   }
 
-  private static native void nativeSetup(long jsiPtr);
+  private static native void nativeSetup(long jsiPtr, CallInvokerHolderImpl jsCallInvokerHolder, String temporaryDirectory);
 
   @Override
   public boolean setup() {
-    nativeSetup(getReactApplicationContext().getJavaScriptContextHolder().get());
+    nativeSetup(
+      getReactApplicationContext().getJavaScriptContextHolder().get(),
+      (CallInvokerHolderImpl)getReactApplicationContext().getCatalystInstance().getJSCallInvokerHolder(),
+      getReactApplicationContext().getCacheDir().getAbsolutePath()
+    );
     return true;
   }
 }
